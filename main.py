4d1514b0a9a5689c142ce244a51ce6bc1642ebdb
yepdev <yep.dev7@gmail.com>
2022-09-19 01:08:07 +0200

    Delete dangligh highlights files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import json
+from os import listdir
+from os.path import isfile, join
 
 from fastapi import FastAPI
 from starlette.requests import Request
@@ -15,10 +17,11 @@ output_folder = f'{os.path.expanduser("~")}/Drive/sync/obsidian/Memex/'
 
 @app.post("/load")
 async def load(request: Request):
+    # -------- series handling --------
     with open('data/series.json') as file:
         series_data = json.load(file)
         for key, value in series_data.items():
-            series_data[key]['articles'] = [url.lstrip('https://www.').rstrip('/') for url in value['articles']]
+            series_data[key]['articles'] = [url.replace('https://www.', "").rstrip('/') for url in value['articles']]
     for name in series_data:
         pathlib.Path(output_folder + name).mkdir(exist_ok=True)
     reversed_series = {}
@@ -26,9 +29,12 @@ async def load(request: Request):
         for url in value['articles']:
             reversed_series[url] = key
 
-    article = await request.json()
-    for key, value in article.items():
-        title = value['title']
+    # -------- writing articles files --------
+    titles = []
+    articles = await request.json()
+    for key, article in articles.items():
+        title = article['title']
+        titles.append(title)
 
         series = reversed_series.get(key, '')
         if series:
@@ -37,12 +43,19 @@ async def load(request: Request):
             series = series + '/'
             title = f"{position:02d} {title}"
 
-        value['annotations'].sort(key=lambda x: x['position'])
+        article['annotations'].sort(key=lambda x: x['position'])
         with open(f"{output_folder}{series}{title}.md", "w") as f:
             f.write(template.render(
-                **value,
+                **article,
                 excaped_title=title.replace("'", "''"),
                 url=key,
                 slug=slugify(title)
             ))
+
+    # -------- removing files for not passed articles --------
+    files = [file[:-3] for file in listdir(output_folder) if
+             isfile(join(output_folder, file)) and not file.startswith('_')]
+    for file in set(files) - set(titles):
+        os.remove(f"{output_folder}{file}.md")
+
     return {}
